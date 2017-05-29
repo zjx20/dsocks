03c1c071f8bcad213ba9ab2f49da3c15b1ea5ca2
zjx20 <zhoujianxiong2@gmail.com>
2017-05-29 22:58:37 +0800

    Links the compiled library as libdsocks.so

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# socksify with dsocks for OSX
 
 VERSION=1.0
 TARGET=
@@ -7,15 +6,16 @@ FILES=
 FLAGS=
 
 if [ `uname -s` = "Darwin" ]; then
-  TARGET=libdsocks.dylib.${VERSION}
+  TARGET=libdsocks.dylib
   FILES="dsocks.c atomicio.c"
   FLAGS="-g -Wall -Wextra -O2 -fPIC --shared -ldl -lresolv"
 else
-  TARGET=libdsocks.so.${VERSION}
+  TARGET=libdsocks.so
   FILES="dsocks.c atomicio.c strlcpy.c"
   FLAGS="-g -Wall -Wextra -O2 -fPIC --shared -Wl,--version-script,libdsocks.map -ldl -lresolv"
 fi
 
-if [ ! -f ${TARGET} ] || [ "$1" = "-f" ]; then
-  gcc ${FLAGS} ${FILES} -o ${TARGET}
+if [ ! -f ${TARGET}.${VERSION} ] || [ "$1" = "-f" ]; then
+  gcc ${FLAGS} ${FILES} -o ${TARGET}.${VERSION}
+  [ "$?" = "0" ] && ln -sf ${TARGET}.${VERSION} libdsocks.so
 fi
